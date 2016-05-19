64c239d2ea324800d5394faf72e9c7c0b8c80fb4
Lucas Sinclair <lucas.sinclair@me.com>
2016-05-19 22:28:24 +0200

    Update mysterious sample

--- scripts/projects/micans/micans_v6/load.py
+++ scripts/projects/micans/micans_v6/load.py
@@ -19,7 +19,7 @@ from sifes.demultiplex.demultiplexer import Demultiplexer
 plexed = sifes.load("~/deploy/sifes/metadata/json/projects/micans/micans_v6_exp1_plexed/")
 
 # Load all real project #
-projects = ['minican_4_5', 'posmic_olk', 'pseud_fluo', 'febex_dp', 'cosc_1']
+projects = ['minican_4_5', 'posmic_olk', 'pseud_fluo', 'febex_dp', 'cosc_1', 'mysterious']
 projects = [sifes.load("~/deploy/sifes/metadata/json/projects/micans/" + p, False) for p in projects]
 samples  = [s for p in projects for s in p]
 samples.sort(key=lambda s: s.num)
