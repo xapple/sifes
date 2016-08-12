0286e7e67e301e906bf58bc96adedf6f42646e2e
Lucas Sinclair <lucas.sinclair@me.com>
2016-08-12 16:05:20 +0200

    Rerun with more primer mismatches

--- scripts/projects/uppsala_universitet/heli_hundred_lakes/load.py
+++ scripts/projects/uppsala_universitet/heli_hundred_lakes/load.py
@@ -20,8 +20,8 @@ import sifes
 proj = sifes.load("~/deploy/sifes/metadata/json/projects/uppsala_universitet/hundred_lakes_euk/")
 
 # Parameters #
-sifes.filtering.seq_filter.SeqFilter.primer_mismatches = 1
-sifes.filtering.seq_filter.SeqFilter.primer_max_dist   = 70
-sifes.filtering.seq_filter.SeqFilter.min_read_length   = 300
-sifes.filtering.seq_filter.SeqFilter.max_read_length   = 560
+sifes.filtering.seq_filter.SeqFilter.primer_mismatches = 2
+sifes.filtering.seq_filter.SeqFilter.primer_max_dist   = 35
+sifes.filtering.seq_filter.SeqFilter.min_read_length   = 400
+sifes.filtering.seq_filter.SeqFilter.max_read_length   = 530
 for s in proj: s.default_joiner = 'pandaseq'
--- scripts/projects/uppsala_universitet/heli_hundred_lakes/run_all.py
+++ scripts/projects/uppsala_universitet/heli_hundred_lakes/run_all.py
@@ -24,66 +24,65 @@ from tqdm import tqdm
 proj = sifes.load("~/deploy/sifes/metadata/json/projects/uppsala_universitet/hundred_lakes_euk/")
 
 # Parameters #
-sifes.filtering.seq_filter.SeqFilter.primer_mismatches = 1
-sifes.filtering.seq_filter.SeqFilter.primer_max_dist   = 70
-sifes.filtering.seq_filter.SeqFilter.min_read_length   = 300
-sifes.filtering.seq_filter.SeqFilter.max_read_length   = 560
+sifes.filtering.seq_filter.SeqFilter.primer_mismatches = 2
+sifes.filtering.seq_filter.SeqFilter.primer_max_dist   = 35
+sifes.filtering.seq_filter.SeqFilter.min_read_length   = 400
+sifes.filtering.seq_filter.SeqFilter.max_read_length   = 530
 for s in proj: s.default_joiner = 'pandaseq'
 
 ###############################################################################
-# Get information for excel file #
+print("# Get information for excel file #")
 for s in proj: print s.pair.fwd.count
 for s in proj: print s.pair.rev.count
 for s in proj: print s.pair.fwd.md5
 for s in proj: print s.pair.rev.md5
 for s in proj: print len(s.pair.fwd.first)
 
-# Uncompress reads - 0h03 #
+print("# Uncompress reads - 0h03 #")
 with Timer(): prll_map(lambda s: s.uncompressed_pair, proj)
 for s in proj:
     assert s.uncompressed_pair.fwd.count == s.uncompressed_pair.rev.count
     assert s.pair.count                  == s.uncompressed_pair.rev.count
 
-# Join reads - 0h02 #
+print("# Join reads - 0h02 #")
 with Timer(): prll_map(lambda s: s.joiner.run(cpus=1), proj)
 for s in proj: print s.short_name, s.joiner.results.unassembled_percent
-for s in proj: print s.joiner.results.assembled.graphs.length_dist()
 
-# Filter - 0h03 #
+print("# Filter - 0h03 #")
 with Timer(): prll_map(lambda s: s.filter.run(), proj)
 for s in proj: print s.short_name, s.filter.primers_fasta.count
 for s in proj: print s.short_name, s.filter.n_base_fasta.count
 for s in proj: print s.short_name, s.filter.length_fasta.count
 for s in proj: print s.short_name, s.filter.renamed_fasta.count
 
-# Cluster combine reads - 0h01 #
+print("# Cluster combine reads - 0h01 #")
 print proj.short_name, proj.cluster.num_dropped_samples
 print proj.short_name, [s.clean.count for s in proj.cluster.good_samples]
 print proj.short_name, [s.clean.count for s in proj.cluster.bad_samples]
 print proj.short_name, proj.cluster.read_count_cutoff
 with Timer(): proj.cluster.combine_reads()
 
-# Make centers  - 0h05 #
+print("# Make centers  - 0h05 #")
 with Timer(): proj.cluster.centering.run(cpus=32)
 print proj.short_name, proj.cluster.centering.results.centers.count
 
-# Taxonomy assignment - 0h07 #
+print("# Taxonomy assignment - 0h07 #")
 with Timer(): proj.cluster.taxonomy.run(cpus=1)
 print proj.short_name, len(proj.cluster.taxonomy.results.assignments)
 
-# Make the OTU table - 0h01 #
+print("# Make the OTU table - 0h01 #")
 with Timer(): proj.cluster.otu_table.run()
 
-# Make the taxa tables - 0h01 #
+print("# Make the taxa tables - 0h01 #")
 with Timer(): proj.cluster.taxa_table.run()
 
-# Make fastqc graphs - 0h50 #
+print("# Make fastqc graphs - 0h50 #")
 for s in tqdm(proj):
     print "fastqc101 on sample '%s'" % s
     s.pair.fwd.fastqc.run()
     s.pair.rev.fastqc.run()
 
-# Make diversity sample graphs - 0h03 #
+print("# Make diversity sample graphs - 0h03 #")
 def diversity_plot(s):
     s.graphs.chao1(rerun=True)
     s.graphs.ace(rerun=True)
@@ -91,7 +90,7 @@ def diversity_plot(s):
     s.graphs.simpson(rerun=True)
 with Timer(): prll_map(diversity_plot, proj)
 
-# Make project graphs - 0h13 #
+print("# Make project graphs - 0h13 #")
 def otu_plot(p):
     p.cluster.otu_table.results.graphs.otu_sizes_dist(rerun=True)
     p.cluster.otu_table.results.graphs.otu_sums_graph(rerun=True)
@@ -103,27 +102,33 @@ def otu_plot(p):
     p.cluster.nmds_graph(rerun=True)
 with Timer(): otu_plot(proj)
 
-# Optionally clean cache #
+print("# Make length dist graphs #")
+for s in proj: print s.joiner.results.assembled.graphs.length_dist(rerun=True)
+
+print("# Optionally clean cache #")
 for s in proj: s.report.cache_dir.remove()
 for s in proj: s.report.cache_dir.create()
 for s in proj: print FilePath(s.report.cache_dir + 'genera_table.pickle').remove()
 
-# Make cluster reports - 0h07 #
+print("# Make cluster reports - 0h07 #")
 with Timer(): proj.cluster.report.generate()
 
-# Make sample reports - 0h0x #
+print("# Attribute deletion because of odd pickling parallelisation problem #")
+for s in proj: del s.joiner.results.assembled.graphs
+
+print("# Make sample reports - 0h05 #")
 with Timer(): prll_map(lambda s: s.report.generate(), proj)
 
-# Bundle - 0h02 #
+print("# Bundle - 0h02 #")
 from sifes.distribute.bundle import Bundle
 bundle = Bundle("hundred_lakes", proj.samples)
 with Timer(): bundle.run()
 
-# Extra files #
+print("# Extra files #")
 path = sifes.home + "deploy/sifes/metadata/excel/projects/uppsala_universitet/heli_hundred_lakes/metadata.xlsx"
 shutil.copy(path, bundle.p.samples_xlsx)
 
-# Upload - 0h00 #
+print("# Upload - 0h02 #")
 from sifes.distribute.upload import DropBoxSync
 dbx_sync = DropBoxSync(bundle.base_dir, '/100 lakes analysis delivery')
 with Timer(): dbx_sync.run()
