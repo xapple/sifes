6d69bf5a7f153f7fc57ef6f7c4a0e0b21b6b9483
Lucas Sinclair <lucas.sinclair@me.com>
2017-04-10 15:26:46 +0200

    Adding foram metadata bis

---
+++ scripts/projects/unige/foram/run_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+A script to run all the procedure on our foram desalt project.
+
+To first generate the JSON files:
+
+    $ ~/repos/sifes/metadata/excel_to_json.py ~/repos/sifes/metadata/excel/projects/unige/foram_plexed/metadata_plexed.xlsx
+    $ ~/repos/sifes/metadata/excel_to_json.py ~/repos/sifes/metadata/excel/projects/unige/foram/metadata.xlsx
+
+"""
