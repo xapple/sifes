424c7de047eca83b33ec4dc0715202176208d286
Lucas Sinclair <lucas.sinclair@me.com>
2017-06-01 11:01:44 +0200

    rda files

---
+++ scripts/projects/unige/desalt/redundancy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+# Load #
+import os
+home = os.environ.get('HOME', '~') + '/'
+execfile(home + "deploy/sifes/scripts/projects/unige/desalt/load.py")
